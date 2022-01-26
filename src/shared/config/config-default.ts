0f7403cc29c77d36d113baa60761e2162a134581
ribas89 <ribas89@pm.me>
2022-01-26 20:43:58 -0300

    fix: remove utils circular import warning

--- src/shared/config/config-default.ts
+++ src/shared/config/config-default.ts
@@ -1,10 +1,78 @@
+import _ from 'lodash';
+import { Dimensions } from 'react-native';
+
 import {
   DEFAULT_COLORS,
   DEFAULT_FONTS,
   DEFAULT_GENERIC_ALIAS,
+  DEFAULT_JS_COLORS,
+  REGEX_HEX,
 } from '../constants';
-import { getColor, scale, vscale } from '../utils';
-import { AbstractConfig, ConfigProps } from './config-class';
+import { AbstractConfig, ConfigInstance, ConfigProps } from './config-class';
+
+export function relativePixel({
+  value,
+  realDimension,
+  scaledDimension,
+}: {
+  value?: any;
+  realDimension: number;
+  scaledDimension: number;
+}) {
+  if (!value) return 0;
+  const numberValue = Number(value);
+  if (isNaN(numberValue)) return 0;
+
+  const scaled = Math.round((realDimension / scaledDimension) * numberValue);
+  return !!scaled ? scaled : 1;
+}
+
+export function scale(value: any, config: ConfigInstance) {
+  const { width } = Dimensions.get('window');
+  return relativePixel({
+    value,
+    realDimension: width,
+    scaledDimension: config.width,
+  });
+}
+
+export function vscale(value: any, config: ConfigInstance): number {
+  const { height } = Dimensions.get('window');
+  return relativePixel({
+    value,
+    realDimension: height,
+    scaledDimension: config.height,
+  });
+}
+
+export function getAlpha(value: number): string {
+  const _alpha = Number(value) / 100;
+  return Math.floor(_alpha * 255)
+    .toString(16)
+    .padStart(2, '0');
+}
+
+export function getColor(color: string, config: ConfigInstance) {
+  let _color = color;
+  let alphaValue = '';
+
+  const alphaMatch = _color.match(/^(.+):alpha:(\d+)$/);
+  if (alphaMatch) {
+    _color = alphaMatch[1];
+    const alpha = Number(alphaMatch[2]);
+    alphaValue = !!alpha ? getAlpha(alpha) : '';
+  }
+
+  if (REGEX_HEX.exec(_color)) return _color + alphaValue;
+
+  const colorHex = _.get(config.colors, _color, false);
+  if (colorHex) return colorHex + alphaValue;
+
+  const colorHexJs = (DEFAULT_JS_COLORS as any)[_color];
+  if (colorHexJs) return colorHexJs + alphaValue;
+
+  return _color;
+}
 
 const keyPairType = function (value: any) {
   const clone = JSON.parse(JSON.stringify(value));
--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,8 +1,4 @@
-import _ from 'lodash';
-import { Dimensions } from 'react-native';
-
 import { ConfigInstance, defaultConfig } from './config';
-import { DEFAULT_JS_COLORS, REGEX_HEX } from './constants';
 
 import type { StyleResult } from './types';
 export const kebabToCamelCase = (s: string) =>
@@ -28,67 +24,3 @@ export const getEndValue = (
 
   return _config.scale(numberValue);
 };
-
-export function relativePixel({
-  value,
-  realDimension,
-  scaledDimension,
-}: {
-  value?: any;
-  realDimension: number;
-  scaledDimension: number;
-}) {
-  if (!value) return 0;
-  const numberValue = Number(value);
-  if (isNaN(numberValue)) return 0;
-
-  const scaled = Math.round((realDimension / scaledDimension) * numberValue);
-  return !!scaled ? scaled : 1;
-}
-
-export function scale(value: any, config: ConfigInstance) {
-  const { width } = Dimensions.get('window');
-  return relativePixel({
-    value,
-    realDimension: width,
-    scaledDimension: config.width,
-  });
-}
-
-export function vscale(value: any, config: ConfigInstance): number {
-  const { height } = Dimensions.get('window');
-  return relativePixel({
-    value,
-    realDimension: height,
-    scaledDimension: config.height,
-  });
-}
-
-export function getAlpha(value: number): string {
-  const _alpha = Number(value) / 100;
-  return Math.floor(_alpha * 255)
-    .toString(16)
-    .padStart(2, '0');
-}
-
-export function getColor(color: string, config: ConfigInstance) {
-  let _color = color;
-  let alphaValue = '';
-
-  const alphaMatch = _color.match(/^(.+):alpha:(\d+)$/);
-  if (alphaMatch) {
-    _color = alphaMatch[1];
-    const alpha = Number(alphaMatch[2]);
-    alphaValue = !!alpha ? getAlpha(alpha) : '';
-  }
-
-  if (REGEX_HEX.exec(_color)) return _color + alphaValue;
-
-  const colorHex = _.get(config.colors, _color, false);
-  if (colorHex) return colorHex + alphaValue;
-
-  const colorHexJs = (DEFAULT_JS_COLORS as any)[_color];
-  if (colorHexJs) return colorHexJs + alphaValue;
-
-  return _color;
-}
